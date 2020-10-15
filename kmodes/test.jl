f0d8b43e95bc71db691c2ef31b00beaec44d86b9
Fisher Moritzburke <fmoritzburke@gmail.com>
2020-10-15 14:19:08 -0700

    more tests

--- kmodes/test.jl
+++ kmodes/test.jl
@@ -4,8 +4,8 @@ using Test
 include("kmodes.jl")
 kmodes = KModes.kmodes
 
-@testset "basic single centroid" begin
-    X = [4 4 5]
+@testset "single centroid" begin
+    X = [3 4 5]
     res = kmodes(X, 1)
     @test res.assignments = [1,1,1]
     @test in(res.centroids[1,1], X)
@@ -13,16 +13,30 @@ kmodes = KModes.kmodes
     @test res.converged
 end
 
-@testset "basic two centroids" begin
-    X = [4 4 5]
+@testset "two centroids, manual centroid init" begin
+    X = [3 4 5]
     res = kmodes(X, 2; init=[3 6])
     @test res.cost == 0
     @test res.converged
 end
 
+@testset "two centroids, Huang centroid init" begin
+    X = [3 4 5]
+    res = kmodes(X, 2; init_alg=KModes.huang_centroid_init)
+    @test res.cost == 0
+    @test res.converged
+end
+
+@testset "two centroids, random centroid init" begin
+    X = [3 4 5]
+    res = kmodes(X, 2; init_alg=KModes.random_centroid_init)
+    @test res.cost == 0
+    @test res.converged
+end
+
 @testset "no convergence/max_iter" begin
-    X = [4 5 6; 1 2 3]
-    res = kmodes(X, 2; init=[0 0; 0 0], max_iter=1)
+    X = [4 5 5; 1 2 3]
+    res = kmodes(X, 2; init=[0 0; 7 7], max_iter=0)
     @test !res.converged
 end
 
